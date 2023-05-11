73ceb51d092c48fa520f552e1dc51b60a35bc1c8
alikaba85 <63543118+alikaba85@users.noreply.github.com>
2023-05-11 22:46:04 +0300

    Proje dosyası ekle.

---
+++ EasyCashIdentityProject.BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
+using FluentValidation;
+
+namespace EasyCashIdentityProject.BusinessLayer.ValidationRules.AppUserValidationRules
+{
+    public class AppUserRegisterValidator : AbstractValidator<AppUserRegisterDto>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Ad Alanı Boş Geçilemez");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Soyad Alanı Boş Geçilemez");
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("Username Alanı Boş Geçilemez");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Mail Alanı Boş Geçilemez");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Şifre Alanı Boş Geçilemez");
+            RuleFor(x => x.ComfirmPassword).NotEmpty().WithMessage("Şifre Tekrar Alanı Boş Geçilemez");
+            RuleFor(x => x.Name).MaximumLength(30).WithMessage("Lütfen En Fazla 30 Karakter Giriniz");
+            RuleFor(x => x.Name).MinimumLength(3).WithMessage("Lütfen En Az 3 Karakter Giriniz ");
+            RuleFor(x => x.ComfirmPassword).Equal(y => y.Password).WithMessage("Şifreniz Eşleşmiyor");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Lütfen Geçerli Bir Mail Adresi Giriniz");
+        }
+    }
+}
---
+++ EasyCashIdentityProject.PresantationLayer/Controllers/RegisterController.cs
@@ -0,0 +1,58 @@
+﻿using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresantationLayer.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult> Index(AppUserRegisterDto appUserRegisterDto)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser()
+                {
+                    Name = appUserRegisterDto.Name,
+                    Surname = appUserRegisterDto.Surname,
+                    UserName = appUserRegisterDto.UserName,
+                    Email = appUserRegisterDto.Email,
+                    City = "aaaa",
+                    District = "bbbb",
+                    ImageUrl = "ccbc"
+                };
+                var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "ComfirmMail");
+                }
+                else
+                {
+                    foreach (var item  in  result.Errors)
+                    {
+                        ModelState.AddModelError("",item.Description);
+                    }
+                }
+
+            }
+
+            return View();
+        }
+    }
+}
